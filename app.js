3efe36062e4758efff9a268021362cea0725eb96
jiohc <85597770+jiohc@users.noreply.github.com>
2021-10-04 16:47:10 -0700

    add when statement for about me

--- app.js
+++ app.js
@@ -31,7 +31,20 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         }
     ])
 }; 
