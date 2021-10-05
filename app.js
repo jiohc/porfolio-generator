0593985937ab1e464967656975243049f10d11c2
jiohc <85597770+jiohc@users.noreply.github.com>
2021-10-05 12:48:55 -0700

    updated HTML template

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -29,6 +33,12 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
@@ -39,12 +49,6 @@ const promptUser = () => {
                     return false;
                 }
             }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "About" section?',
-            default: true
         }
     ])
 }; 
@@ -132,19 +136,13 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error (err);
+
+          console.log('Portfolio complete! Check out index.html to see the output!');
+        });
 
     });
-    
-
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
